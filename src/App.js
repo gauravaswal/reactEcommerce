14a2a99b5bc3b1f992e664fe3df2be89dc31aa88
Rajat Thakur <rajat@techindustan.com>
2022-03-01 21:45:18 +0530

    folder structure refactored

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import Slider from "react-slick";
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import Navbar from './components/navbar';
-import Heading from './components/Heading';
-import Sliders from './components/Slider';
-import Main from './Main';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Navbar from "./components/navbar";
+import Heading from "./components/Heading";
+import Sliders from "./components/Slider";
+import Main from "./pages/Main";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link
-} from "react-router-dom";
-import SecondNavbar from './components/SecondNavbar';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import SecondNavbar from "./components/SecondNavbar";
 function App() {
-  return (
-   <Routes>
-          <Route path="/" element={<Main />}>
-          </Route>
-          </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Main />}></Route>
+		</Routes>
+	);
 }
 
 export default App;
